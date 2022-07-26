396edbaf7a406442679fb229f97b0841f74b54bb
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2022-07-26 12:41:02 +0200

    logo

---
+++ front/src/components/Shared/logo/Logo.js
@@ -0,0 +1,10 @@
+import logo from '../../../assets/images/ugoLogin.png';
+
+const Logo = () => {
+    return (
+        <div>
+            <img className='w-56 h-auto pb-4 mx-auto' src={logo} alt='Hive Space Logo' loading='lazy' />
+        </div>
+    )
+}
+export default Logo;
