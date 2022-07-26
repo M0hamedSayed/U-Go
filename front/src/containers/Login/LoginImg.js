a5428061e6c95b3455a2db36a8de7c36ebfe3708
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2022-07-26 12:44:44 +0200

    login image and form

---
+++ front/src/containers/Login/LoginImg.js
@@ -0,0 +1,34 @@
+import { css } from '@emotion/css';
+import SvgLogin from '../../components/Login/SvgLogin';
+import bgLogin from '../../assets/images/bg-auth.png';
+import manLogin from '../../assets/images/man-auth.png';
+import useI18next from '../../Hooks/useI18next';
+
+const LoginImg = () => {
+    const [dir] = useI18next();
+
+    const rotate = dir === 'ltr' ? 'rotateY(0deg)' : 'rotateY(180deg)';
+
+    const main_register = css`
+        background-image: url(${manLogin});
+        background-size: contain;
+        background-repeat: no-repeat;
+    `
+    const container_signLeft = css`
+    background-image: url(${bgLogin});
+    background-size: 100% 100%;
+    background-repeat: no-repeat;
+    transform:${rotate};
+    `
+
+    return (
+        <section className={`${container_signLeft}  hidden lg:flex w-3/5 h-screen justify-center items-center py-14 px-14 relative`}>
+            <div className='absolute -right-8 w-40 h-60'>
+                <div className={`${main_register} w-full h-full`} ></div>
+            </div>
+            <SvgLogin />
+        </section>
+
+    );
+}
+export default LoginImg;
