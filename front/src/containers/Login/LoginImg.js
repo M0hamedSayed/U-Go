aaed1951d9b9b61f8aae1b50f473a223c9e291da
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2022-07-26 16:48:28 +0200

    loaded images before set it in background CSS

--- front/src/containers/Login/LoginImg.js
+++ front/src/containers/Login/LoginImg.js
@@ -3,19 +3,22 @@ import SvgLogin from '../../components/Login/SvgLogin';
 import bgLogin from '../../assets/images/bg-auth.png';
 import manLogin from '../../assets/images/man-auth.png';
 import useI18next from '../../Hooks/useI18next';
+import useProgressiveImage from './../../Hooks/useProgressiveImage';
+
 
 const LoginImg = () => {
     const [dir] = useI18next();
-
+    const loadedBgLogin = useProgressiveImage(bgLogin);
+    const loadedManLogin = useProgressiveImage(manLogin);
     const rotate = dir === 'ltr' ? 'rotateY(0deg)' : 'rotateY(180deg)';
 
     const main_register = css`
-        background-image: url(${manLogin});
+        background-image: url(${loadedManLogin || manLogin});
         background-size: contain;
         background-repeat: no-repeat;
     `
     const container_signLeft = css`
-    background-image: url(${bgLogin});
+    background-image: url(${loadedBgLogin || bgLogin});
     background-size: 100% 100%;
     background-repeat: no-repeat;
     transform:${rotate};
@@ -28,7 +31,6 @@ const LoginImg = () => {
             </div>
             <SvgLogin />
         </section>
-
     );
 }
 export default LoginImg;
